c407aa4ecfd21da6d161bd2cd30e337bc6d5533e
Pdevshali <prashantdevshali0099@gmail.com>
2025-06-28 13:48:09 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import helper
+import pickle
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.header("Duplicate Question pairs Detection")
+
+q1 = st.text_input("Enter Question 1")
+q2 = st.text_input("Enter Question 2")
+
+
+if st.button("Find"):
+    query = helper.query_point_creator(q1, q2)
+    result = model.predict(query)[0]    # ✅ Correct
+
+    if result:
+        st.header("The questions are duplicates.")
+    else:
+        st.header("The questions are not duplicates.")
